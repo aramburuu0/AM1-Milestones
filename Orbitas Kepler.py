1f9ae35de5f540e5671d34b12b5fd15744ffa9bf
aramburuu0 <aramburuiker@gmail.com>
2024-10-08 17:12:30 +0200

    Orbitas de Kepler primer dia actualizado

--- Orbitas Kepler.py
+++ Orbitas Kepler.py
@@ -17,20 +17,21 @@ deltat = T / N
 # Listas para almacenar los valores de x e y
 x = zeros(N+1) # Inicializa un array de tamaño N+1
 y = zeros(N+1)
-
+xd = zeros(N+1)
+yd = zeros(N+1)
 # Condiciones iniciales
 x[0] = 1
 y[0] = 0
-xd = 0
-yd = 1
+xd[0] = 0
+yd[0] = 1
 
 # Bucle para la integración numérica
 for i in range(N):
-    F1, F2, F3, F4 = F(x[i], y[i], xd, yd)
+    F1, F2, F3, F4 = F(x[i], y[i], xd[i], yd[i])
     x[i+1] = x[i] + deltat * F1
     y[i+1] = y[i] + deltat * F2
-    xd = xd + deltat * F3
-    yd = yd + deltat * F4
+    xd[i+1] = xd[i] + deltat * F3
+    yd[i+1] = yd[i] + deltat * F4
     
 
 # Representación gráfica
@@ -40,4 +41,12 @@ plt.ylabel('y')
 plt.title('Trayectoria en 2D')
 plt.grid(True)
 plt.axis('equal')  # Escala igual en ambos ejes
+plt.show()
+
+plt.plot(xd,yd)
+plt.xlabel('Velocidad en x')
+plt.ylabel('Velocidad en y')
+plt.title('Componentes velocidad')
+plt.grid(True)
+plt.axis('equal')
 plt.show()
