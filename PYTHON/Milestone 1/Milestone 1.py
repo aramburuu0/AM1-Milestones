7d9537b3a9609fca6d29185d8a67b1b89c38065a
aramburuu0 <aramburuiker@gmail.com>
2024-10-11 21:24:18 +0200

    EE, RK2 y RK4

--- PYTHON/Milestone 1/Milestone 1.py
+++ PYTHON/Milestone 1/Milestone 1.py
@@ -53,10 +53,8 @@ for n in range(0, N):
     F_rk2[n, 1] = U_rk2[n, 3]
     F_rk2[n, 2] = -U_rk2[n, 0]/(U_rk2[n, 0]**2 + U_rk2[n, 1]**2)**(3/2)
     F_rk2[n, 3] = -U_rk2[n, 1]/(U_rk2[n, 0]**2 + U_rk2[n, 1]**2)**(3/2)
-    
-    t[n+1] = t[n] + deltat
-    
-    k1_rk2[n, :] = F_rk2[n, :] 
+     
+    k1_rk2[n, :] = F_rk2[n, :]
 
     k2_rk2[n, 0] = U_rk2[n, 2] + k1_rk2[n, 2] * (t[n+1] - t[n])
     k2_rk2[n, 1] = U_rk2[n, 3] + k1_rk2[n, 3] * (t[n+1] - t[n])
@@ -65,17 +63,55 @@ for n in range(0, N):
 
     U_rk2[n+1, :] = U_rk2[n, :] + deltat/2 * (k1_rk2[n, :] +  k2_rk2[n, :])
 
+######### RUNGE-KUTTA 4 ETAPAS RK4 #########
+
+U_rk4 = zeros([N+1,len(U0_kepler)])
+F_rk4 = zeros([N+1,len(U0_kepler)])
+k1_rk4 = zeros([N+1,len(U0_kepler)])
+k2_rk4 = zeros([N+1,len(U0_kepler)])
+k3_rk4 = zeros([N+1,len(U0_kepler)])
+k4_rk4 = zeros([N+1,len(U0_kepler)])
+
+U_rk4[0, :] = U0_kepler
+
+for n in range (0, N):
+
+    F_rk4[n, 0] = U_rk4[n, 2]
+    F_rk4[n, 1] = U_rk4[n, 3]
+    F_rk4[n, 2] = -U_rk4[n, 0]/(U_rk4[n, 0]**2 + U_rk4[n, 1]**2)**(3/2)
+    F_rk4[n, 3] = -U_rk4[n, 1]/(U_rk4[n, 0]**2 + U_rk4[n, 1]**2)**(3/2)
+     
+    k1_rk4[n, :] = F_rk4[n, :]
+
+    k2_rk4[n, 0] = U_rk4[n, 2] + k1_rk4[n, 2] * (t[n+1] - t[n])/2
+    k2_rk4[n, 1] = U_rk4[n, 3] + k1_rk4[n, 3] * (t[n+1] - t[n])/2
+    k2_rk4[n, 2] = -(U_rk4[n, 0] + k1_rk4[n, 0] * (t[n+1] - t[n])/2) / ((U_rk4[n, 0] + k1_rk4[n, 0] * (t[n+1] - t[n])/2)**2 + (U_rk4[n, 1] + k1_rk4[n, 1] * (t[n+1] - t[n])/2)**2)**(3/2)
+    k2_rk4[n, 3] = -(U_rk4[n, 1] + k1_rk4[n, 1] * (t[n+1] - t[n])/2) / ((U_rk4[n, 0] + k1_rk4[n, 0] * (t[n+1] - t[n])/2)**2 + (U_rk4[n, 1] + k1_rk4[n, 1] * (t[n+1] - t[n])/2)**2)**(3/2)
+
+    k3_rk4[n, 0] = U_rk4[n, 2] + k2_rk4[n, 2] * (t[n+1] - t[n])/2
+    k3_rk4[n, 1] = U_rk4[n, 3] + k2_rk4[n, 3] * (t[n+1] - t[n])/2
+    k3_rk4[n, 2] = -(U_rk4[n, 0] + k2_rk4[n, 0] * (t[n+1] - t[n])/2) / ((U_rk4[n, 0] + k2_rk4[n, 0] * (t[n+1] - t[n])/2)**2 + (U_rk4[n, 1] + k2_rk4[n, 1] * (t[n+1] - t[n])/2)**2)**(3/2)
+    k3_rk4[n, 3] = -(U_rk4[n, 1] + k2_rk4[n, 1] * (t[n+1] - t[n])/2) / ((U_rk4[n, 0] + k2_rk4[n, 0] * (t[n+1] - t[n])/2)**2 + (U_rk4[n, 1] + k2_rk4[n, 1] * (t[n+1] - t[n])/2)**2)**(3/2)
+
+    k4_rk4[n, 0] = U_rk4[n, 2] + k3_rk4[n, 2] * (t[n+1] - t[n])
+    k4_rk4[n, 1] = U_rk4[n, 3] + k3_rk4[n, 3] * (t[n+1] - t[n])
+    k4_rk4[n, 2] = -(U_rk4[n, 0] + k3_rk4[n, 0] * (t[n+1] - t[n])) / ((U_rk4[n, 0] + k3_rk4[n, 0] * (t[n+1] - t[n]))**2 + (U_rk4[n, 1] + k3_rk4[n, 1] * (t[n+1] - t[n]))**2)**(3/2)
+    k4_rk4[n, 3] = -(U_rk4[n, 1] + k3_rk4[n, 1] * (t[n+1] - t[n])) / ((U_rk4[n, 0] + k3_rk4[n, 0] * (t[n+1] - t[n]))**2 + (U_rk4[n, 1] + k3_rk4[n, 1] * (t[n+1] - t[n]))**2)**(3/2)
+
+    U_rk4[n+1, :] = U_rk4[n, :] + deltat/6 * (k1_rk4[n, :] + 2 * k2_rk4[n, :] + 2 * k3_rk4[n, :] + k4_rk4[n, :])
+
 ######### GRAFICAS #########
 
 plt.figure(figsize=(8, 5))
 plt.axis("equal")
 
 plt.plot( U_euler[:, 0], U_euler[:,1], '-b' , lw = 1, label ="Euler explícito" )
-plt.plot( U_rk2[:, 0], U_rk2[:,1], ':m' , lw = 1, label ="Runge-Kutta 2" )
+plt.plot( U_rk2[:, 0], U_rk2[:,1], '-r' , lw = 1, label ="Runge-Kutta 2" )
+plt.plot( U_rk4[:, 0], U_rk4[:,1], ':g' , lw = 2, label ="Runge-Kutta 4" )
 
 plt.legend()
 plt.xlabel( 'Coordenada x' )
 plt.ylabel( 'Coordenada y' )
-plt.title( r'Órbita con distintos esquemas ($\Delta$t={})'.format(round(deltat,3)) )
+plt.title( r'Órbita con distintos esquemas ($\Delta$t={})'.format(round(deltat,2)) )
 plt.grid()
 plt.show()
